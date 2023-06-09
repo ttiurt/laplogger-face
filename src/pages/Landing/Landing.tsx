7d387c47f30c28aa22074ad3f192e1563617557f
ttiurt <truittzach33@gmail.com>
2023-06-09 03:53:59 -0400

    twit link update

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -13,7 +13,7 @@ const Landing = (): JSX.Element => {
         <div className={styles.about}>
           <h2>WHAT IS LAP LOGGER?</h2>
           <p>Lap Logger is currently a website where fans of Formula 1 can post their opinions and ratings on recent races. Eventually the website will be molded into a place where you can log any race you’ve watched, past or present, to your profile to keep track of races you may or may not want to rewatch down the road. The goal is to provide a site similar to Letterboxd or MyAnimeList for the fans of Formula 1.</p>
-          <p>To stay up to date on all things Lap Logger give us a follow over on <a className={styles.twit} href="https://twitter.com/TheLapLogger">Twitter</a></p>
+          <p>To stay up to date on all things Lap Logger give us a follow over on <a className={styles.twit} href="https://twitter.com/TheLapLogger" target="_blank" rel="noopener noreferrer">Twitter</a></p>
         </div>
       </main>
     </>
